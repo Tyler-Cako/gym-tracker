1850bbf3525f966d7e275714412bb1ef6bb88768
Tyler Cako <tylercakotc@gmail.com>
2022-06-02 20:45:16 -0600

    commit

--- server/index.js
+++ server/index.js
@@ -5,18 +5,10 @@ const dotenv = require('dotenv').config()
 const Exercise = require('./models/exercise')
 const port = 5000 || process.env.port
 
-mongoose.connect(`mongodb+srv://${process.env.USERNAME}:${process.env.PASSWORD}@gymtrackcluster.8748p.mongodb.net/?retryWrites=true&w=majority`)
+mongoose.connect(process.env.MONGO_URI)
 
-const benchPress = new Exercise({
-    Type: 'push',
-    Exercise: 'Bench Press',
-    Weight: 115,
-    Reps: 8
-})
-
-benchPress.save().then(result => {
-    console.log('Exercise saved!')
-    console.log(result)
+app.get('/', () => {
+    
 })
 
 app.listen(port, () => {
