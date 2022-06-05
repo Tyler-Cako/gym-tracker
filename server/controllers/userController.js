e1cb6db40e319e0c0b17e7e380b95c36e592f166
Tyler Cako <tylercakotc@gmail.com>
2022-06-05 12:07:12 -0600

    commit

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -6,6 +6,10 @@ const User = require('../models/userModel')
 const registerUser = asyncHandler ( async(req, res) => {
     const { name, email, password } = req.body
 
+    console.log(name)
+    console.log(email)
+    console.log(password)
+
     if(!name || !email || !password ) {
         res.status(400)
         throw new Error('please add all fields')
@@ -23,6 +27,23 @@ const registerUser = asyncHandler ( async(req, res) => {
     // hash password
     const salt = await bcrypt.genSalt(10)
     const hashedPassword = await bcrypt.hash(password, salt)
+
+    const user = await User.create({
+        name,
+        email,
+        password: hashedPassword
+    })
+
+    if(user) {
+        res.status(201).json({
+            _id: user.id,
+             name : user.name,
+             email: user.email
+        })
+    } else {
+        res.status(400)
+        throw new Error('Invalid user data')
+    }
 })
 
 module.exports = {
